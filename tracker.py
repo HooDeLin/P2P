146373f2c67d317fcd09f5bcd47619441e0f4695
HooDeLin <hoodelin0619@gmail.com>
2017-10-24 00:28:59 +0800

    Basic socket

--- tracker.py
+++ tracker.py
@@ -1,2 +1,18 @@
+import socket
+import sys
+
 def start_tracker(settings):
-    print("Fun times tracker times")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print ("Socket created")
+    try:
+        s.bind(("", settings["port"]))
+    except socket.error as msg:
+        print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
+    print 'Socket bind complete'
+    s.listen(10)
+    print 'Socket now listening'
+    while 1:
+        conn, addr = s.accept()
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    s.close()
