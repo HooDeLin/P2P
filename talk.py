328980957441890311b42feb5ecc8027e45df907
joshuaaung <mj_kyawswa@yahoo.com>
2017-10-27 15:37:47 +0800

    complete download and upload for requester and owner

--- talk.py
+++ talk.py
@@ -16,24 +16,26 @@ class Talk:
     def Upload(self, connect):
         path_directory = Path(self.directory)
 
-        # receive the request info fileinfo["filename"], fileinfo["chunk"] (the chunk name), fileinfo["chunkSize"]
-        data = connect.recv(1024)
-        if data:
-            fileInfoMessage = json.loads(data) 
-            print 'Requesting file with the following info: ' + fileInfoMessage
+        # receive the request info fileinfo["chunkFileName"] (the chunk name)
+        data_from_requester = connect.recv(1024)
 
-            file_name = fileInfoMessage["filename"];
-            chunk_name = fileinfo["chunkNumber"];
-            chunk_size = fileInfoMessage["chunkSize"];
+        if data_from_requester:
+            fileInfo = json.loads(data) 
+            print 'Requesting following chunk: ' + fileInfo["chunkFileName"]
 
-            chunk_file_name = chunk_name
+            chunk_file_name = fileinfo["chunkFileName"];
 
             if path_directory.isfile(chunk_file_name):
-                connect.send(json.dumps({"message_type":"FILE_EXISTS"}))
-                response_data = connect.recv(1024)
-                userResponse = json.loads(response_data)
-                if userResponse['message_type'] == 'OK':
-                    with open(file_name, 'rb') as f:
+                message = {}
+                message["message_type"] = "FILE_EXISTS"
+                message["chunkFileSize"] = os.path.getsize(self.directory + "/" + chunk_file_name)
+
+                connect.send(json.dumps(message))
+
+                response_data_from_requester = connect.recv(1024)
+                requester_response = json.loads(response_data_from_requester)
+                if requester_response['message_type'] == 'OK':
+                    with open(chunk_file_name, 'rb') as file_name:
                         bytesToSend = file_name.read(1024)
                         connect.send(bytesToSend)
 
@@ -45,6 +47,11 @@ class Talk:
 
 
     def Download(self, filename):
+        path_directory = Path(self.directory)
+        requested_from_owner_index = []
+
+        sending_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     	# query the tracker who has the chucks to the file by supplying the filename / checksum
     	# message = { "message_type": "QUERY_FILE", "filename": filename }
         # reply = self.send_message_to_tracker(message)
@@ -52,41 +59,72 @@ class Talk:
         message['message_type'] = "QUERY_FILE"
         message['filename'] = filename
         reply = self.send_message_to_tracker(message)
+        
         # Handle "file not found"
         if reply["message_type"] == "QUERY_FILE_ERROR":
             print(reply["error"])
-            return
+            return 
 
-        for index, owner in enumerate(reply['owners'], start=1): 
-            socket = socket.socket()
-            socket.connect(owner)
+        # chunks = {chunk#: ["ip:port", "ip:port"], chunk#: ["ip:port"]}
+        for key, chunkOwners in reply['chunks'].items():
+            chunk_file_name = filename + "." + key + ".chunk"
+            number_of_owners = len(chunkOwners)
 
-            for chunkIndex, chunk in enumerate(reply['chunks'], start=1):
-                message = {}
-                message['filename'] = filename
-                message['chunk'] = chunk
-                message['chunkSize'] = 0
+            if path_directory.isfile(chunk_file_name) 
+                continue;
 
-                socket.send(json.dumps(message)) # send the file info message
-                
-                response_data = connect.recv(1024)
-                peerResponse = json.loads(response_data) #recieve the peer response whether it has the file
+            else:
+                for owner in chunkOwners:
+                    
+                    #randomly choosing host
+                    randomHostIndex = randint(0, number_of_owners-1)
+                    while randomHostIndex in requested_from_owner_index:
+                        randomHostIndex = randint(0, number_of_owners-1)
 
-                if peerResponse['message_type'] == 'FILE_EXISTS':
-                    socket.send(json.dumps({"message_type":"OK"})) #aight! send it over
-                    newFile = open(chunk, 'wb')
-                    data = socket.recv(1024)
-                    totalRecv = len(data)
-                    newFile.write(data)
-                    while totalRecv < filesize: # TODO: I NEED TO KNOW THE FILE SIZE
-                        data = socket.recv(1024)
-                        totalRecv += len(data)
-                        newFile.write(data)
-                    newFile.close() # chunk download completed
+                    requested_from_owner_index.append(randomHostIndex) # already chosen this owner for this chunk
 
-                    #TODO: send message to the tracker (update the tracker with your new update info)
+                    randomHostIPandPort = chunkOwners[randomHostIndex].split(":")
 
-                #continue with another chunk asking the current owner
+                    owner_address = (randomHostIPandPort[0], randomHostIPandPort[1])
+                    sending_socket.connect(owner_address)
+
+                    try:
+                        message = {}
+                        message['chunkFileName'] = chunk_file_name
+
+                        sending_socket.send(json.dumps(message)) # send the file info message to the owner
+                        ownerResponse = sending_socket.recv(1024)
+                        received_data_from_owner = json.loads(ownerResponse)
+                        
+                        # received_data_from_owner['message_type']:string, received_data_from_owner['chunkFileSize']:int
+                        if received_data_from_owner['message_type'] == 'FILE_EXISTS':
+                            chunk_file_size = received_data_from_owner['chunkFileSize']
+
+                            reply_message = {}
+                            reply_message['message_type'] = "OK"
+                            sending_socket.send(json.dumps(reply_message)) #aight! send it over
+
+                            new_chunk_file = open(chunk_file_name, 'wb')
+                            chunk_data_from_owner = sending_socket.recv(1024)
+                            total_received = len(chunk_data_from_owner)
+
+                            new_chunk_file.write(chunk_data_from_owner)
+
+                            while total_received < chunk_file_size:
+                                chunk_data_from_owner = sending_socket.recv(1024)
+                                total_received += len(chunk_data_from_owner)
+                                new_chunk_file.write(chunk_data_from_owner)
+                            new_chunk_file.close()
+
+                            # update tracker
+                            self.register_as_peer()
+
+                        else:
+                            continue
+
+
+                    except:
+                        sending_socket.close()
 
     def listen_for_request():
         self.listening_socket.bind("", self.port)
