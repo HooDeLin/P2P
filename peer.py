08e11d2b6848d49a5302ead4026b6a2988026ea7
HooDeLin <hoodelin0619@gmail.com>
2017-11-06 21:45:04 +0800

    Bug fix 7

--- peer.py
+++ peer.py
@@ -195,11 +195,13 @@ class Peer(Runner):
             print("{}: {}".format(k, v))
 
     def signal_listening(self):
+        self.signal_socket.sendto(json.dumps({"message_type": "GIBERRISH"}), (self.tracker_address, int(self.tracker_signal_port)))
         while True:
             data_received, _ = self.signal_socket.recvfrom(1024)
             try:
                 message = json.loads(data_received)
                 if message["message_type"] == "REQUEST_FILE_CHUNK_SIGNAL":
+                    print("Received request file chunk signal")
                     filename = message["filename"]
                     chunk_number = message["chunk_number"]
                     receiver_address = message["receiver_address"].split(":")
