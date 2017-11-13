abbd218b741cecf3dd08ae11439a4fd0135bc2fc
HooDeLin <hoodelin0619@gmail.com>
2017-11-13 13:32:36 +0800

    Bug fix

--- file_utils.py
+++ file_utils.py
@@ -25,18 +25,14 @@ def combine_chunks(directory, filename, num_of_keys, chunk_size):
 	chunk_index = 0
 	new_file_directory = os.path.join(directory, filename)
 	while(chunk_index < num_of_keys):
-		if str(chunk_index) in reply[MSG_CHUNKS_KEY]:
-			chunk_file_name = filename + "." + str(chunk_index) + ".chunk"
-			chunk_file_directory = os.path.join(directory, chunk_file_name)
+		chunk_file_name = filename + "." + str(chunk_index) + ".chunk"
+		chunk_file_directory = os.path.join(directory, chunk_file_name)
 
-			with open(new_file_directory, 'ab') as new_file:
-				with open(chunk_file_directory, 'rb') as chunk_file:
-					new_file.write(chunk_file.read(chunk_size))
+		with open(new_file_directory, 'ab') as new_file:
+			with open(chunk_file_directory, 'rb') as chunk_file:
+				new_file.write(chunk_file.read(chunk_size))
 
-			chunkIndex+=1
+		chunkIndex+=1
 
-			if(chunkIndex == num_of_keys):
-				remove_all_associated_chunks(directory, filename)
-
-		else:
-			break
+		if(chunkIndex == num_of_keys):
+			remove_all_associated_chunks(directory, filename)
